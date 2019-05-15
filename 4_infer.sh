dbff18085fe021e21c46f0abbe11ce767b1da6f6
me <me@localhost.localdomain>
2019-05-15 18:03:25 -0400

    Version of OpenNMT

--- 4_infer.sh
+++ 4_infer.sh
@@ -4,5 +4,5 @@ source ./venv35/bin/activate
 
 set -x
 
-head -5 ./data/src-test.txt >/tmp/testfile.txt
+head -1 ./data/src-test.txt >/tmp/testfile.txt
 onmt-main infer --auto_config --config data.yml --features_file /tmp/testfile.txt
